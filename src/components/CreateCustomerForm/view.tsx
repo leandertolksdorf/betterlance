051efffda5ebb11d5b7883b50704fb05c777f026
Leander Tolksdorf <leander.tolksdorf@fu-berlin.de>
2022-02-28 20:33:20 +0100

    improve CreateCustomerForm collapse behavior

--- src/components/CreateCustomerForm/view.tsx
+++ src/components/CreateCustomerForm/view.tsx
@@ -1,5 +1,5 @@
 import classNames from "classnames";
-import React, { FormEventHandler } from "react";
+import React, { FormEventHandler, useEffect, useRef, useState } from "react";
 import { FieldErrors, UseFormRegister } from "react-hook-form";
 import { FormData } from ".";
 
@@ -13,13 +13,42 @@ type CreateCustomerFormViewProps = {
 };
 
 export const CreateCustomerFormView = (props: CreateCustomerFormViewProps) => {
+  const innerRef = useRef<HTMLDivElement>(null);
+  const [innerHeight, setInnerHeight] = useState(0);
+
+  useEffect(() => {
+    setInnerHeight(innerRef.current?.scrollHeight || 0);
+  });
   return (
-    <div className={classNames("bg-gray-100", "rounded", "my-2")}>
+    <div
+      className={classNames(
+        "bg-gray-100",
+        "rounded-lg",
+        "my-2",
+        "transition",
+        "ease-in-out",
+        "relative",
+        "shadow-[0_0_100vmax_100vmax_transparent]",
+        props.isOpen && "shadow-black/50"
+      )}
+    >
       <button className={classNames()} onClick={props.onOpen}>
         {props.isOpen ? "Schließen" : "Kund*in anlegen"}
       </button>
-      <div className={classNames(!props.isOpen && "hidden")}>
+      <div
+        ref={innerRef}
+        className={classNames(
+          "overflow-hidden",
+          "transition-[max-height]",
+          "duration-300",
+          "ease-in-out"
+        )}
+        style={{
+          maxHeight: props.isOpen ? innerHeight : 0,
+        }}
+      >
         <div className={classNames("p-4")}>
+          <h3 className={classNames("mb-2")}>Kund*in anlegen</h3>
           <form onSubmit={props.onSubmit}>
             <label>
               Firma
@@ -27,7 +56,7 @@ export const CreateCustomerFormView = (props: CreateCustomerFormViewProps) => {
             </label>
             <label>
               Name
-              <input {...props.register("name")} />
+              <input {...props.register("name", { required: true })} />
             </label>
             <label>
               Email
@@ -37,14 +66,16 @@ export const CreateCustomerFormView = (props: CreateCustomerFormViewProps) => {
               Straße & Hausnummer
               <input {...props.register("address")} />
             </label>
-            <label>
-              Postleitzahl
-              <input {...props.register("zip")} />
-            </label>
-            <label>
-              Stadt
-              <input {...props.register("city")} />
-            </label>
+            <div className={classNames("grid", "grid-cols-2", "gap-4")}>
+              <label className={classNames("col-span-1")}>
+                Postleitzahl
+                <input {...props.register("zip")} />
+              </label>
+              <label className={classNames("col-span-1")}>
+                Stadt
+                <input {...props.register("city")} />
+              </label>
+            </div>
             <label>
               Land
               <input {...props.register("country")} />
