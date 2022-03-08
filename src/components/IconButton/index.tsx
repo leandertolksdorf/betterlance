6425e3972da4b8b5a5253b4c78dc43a7b8b7a5b9
Leander Tolksdorf <leander.tolksdorf@fu-berlin.de>
2022-03-09 00:00:03 +0100

    IconButton: create component (#21)

---
+++ src/components/IconButton/index.tsx
@@ -0,0 +1,58 @@
+import classNames from "classnames";
+import Link from "next/link";
+import { DOMAttributes, ReactNode, useState } from "react";
+import { IconButtonView } from "./view";
+
+export type IconButtonProps = {
+  className?: string;
+  text?: string;
+  icon: ReactNode;
+  color?: "red";
+  alwaysShowText?: boolean;
+  href?: string;
+  onClick?: () => void;
+  onBlur?: DOMAttributes<HTMLButtonElement>["onBlur"];
+};
+
+export const IconButton = (props: IconButtonProps) => {
+  const [showText, setShowText] = useState(props.alwaysShowText || false);
+
+  const onClick = () => {
+    if (showText && props.onClick) {
+      props.onClick();
+    }
+    setShowText(props.alwaysShowText || true);
+  };
+
+  const onBlur = () => {
+    setShowText(false);
+  };
+
+  if (props.href) {
+    return (
+      <Link href={props.href}>
+        <a>
+          <IconButtonView
+            className={props.className}
+            text={props.text}
+            icon={props.icon}
+            color={props.color}
+            showText={showText}
+          />
+        </a>
+      </Link>
+    );
+  } else {
+    return (
+      <button onClick={onClick} onBlur={onBlur}>
+        <IconButtonView
+          className={props.className}
+          text={props.text}
+          icon={props.icon}
+          color={props.color}
+          showText={showText}
+        />
+      </button>
+    );
+  }
+};
---
+++ src/components/IconButton/view.tsx
@@ -0,0 +1,55 @@
+import classNames from "classnames";
+import { DOMAttributes, ReactNode } from "react";
+import { IconButtonProps } from ".";
+
+type IconButtonViewProps = IconButtonProps & {
+  showText: boolean;
+};
+
+export const IconButtonView = (props: IconButtonViewProps) => {
+  return (
+    <div
+      className={classNames(
+        !props.alwaysShowText && props.showText && props.color === "red"
+          ? classNames(
+              "bg-red-300",
+              "text-red-800",
+              "hover:bg-red-400",
+              "hover:text-red-900"
+            )
+          : classNames(
+              "bg-black/10",
+              "text-black/50",
+              "hover:bg-primary-200",
+              "active:text-primary-500"
+            ),
+        "p-2",
+        "w-min",
+        "flex",
+        "justify-center",
+        "items-center",
+        "transition",
+        "rounded-full",
+        "text-xs",
+        props.className
+      )}
+    >
+      {props.text && (
+        <div
+          className={classNames(
+            props.showText ? "max-w-xs" : "max-w-0",
+            "transition-all",
+            "w-auto",
+            "truncate",
+            "overflow-hidden",
+            "uppercase",
+            "font-bold"
+          )}
+        >
+          {props.text}
+        </div>
+      )}
+      <div className={classNames("w-4", "h-4")}>{props.icon}</div>
+    </div>
+  );
+};
