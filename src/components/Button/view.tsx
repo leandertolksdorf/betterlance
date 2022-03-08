213f7c80b6936da374c8f9e490384a7f7fd7c1de
Leander Tolksdorf <leander.tolksdorf@fu-berlin.de>
2022-03-08 20:55:59 +0100

    Button: move styles into component (#17)

--- src/components/Button/view.tsx
+++ src/components/Button/view.tsx
@@ -3,19 +3,38 @@ import { ButtonProps } from ".";
 
 export const ButtonView = (props: ButtonProps) => {
   return (
-    <button
+    <div
       className={classNames(
         props.className,
-        props.light && "light",
-        "flex",
+        props.light
+          ? classNames(
+              "bg-gray-100",
+              "hover:bg-gray-200",
+              "active:bg-gray-300",
+              "text-primary-500"
+            )
+          : classNames(
+              "bg-primary-500",
+              "hover:bg-primary-600",
+              "active:bg-primary-700",
+              "text-white"
+            ),
         props.center && "justify-center",
-        "items-center"
+        "flex",
+        "items-center",
+        "cursor-pointer",
+        "transition",
+        "uppercase",
+        "px-4",
+        "py-2",
+        "rounded",
+        "font-bold"
       )}
     >
       {props.icon && (
         <div className={classNames("inline-icon", "mr-2")}>{props.icon}</div>
       )}
       {props.children}
-    </button>
+    </div>
   );
 };
