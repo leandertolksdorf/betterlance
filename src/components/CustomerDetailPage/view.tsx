f57b996b1256abc38fd10691d84072516cf70b51
Leander Tolksdorf <leander.tolksdorf@fu-berlin.de>
2022-03-08 15:42:56 +0100

    CustomerDetailPage: render edit form only when customer is defined (closes #16)

--- src/components/CustomerDetailPage/view.tsx
+++ src/components/CustomerDetailPage/view.tsx
@@ -36,54 +36,56 @@ export const CustomerDetailPageView = (props: CustomerDetailPageViewProps) => {
         {props.loading || props.customer === undefined ? (
           <Loading />
         ) : (
-          <Box>
-            <div
-              className={classNames(
-                "font-bold",
-                "flex",
-                "items-center",
-                "text-primary-500"
-              )}
-            >
-              <UserIcon className={classNames("inline-icon", "mr-2")} />
-              {props.customer?.name}
-            </div>
-            <div
-              className={classNames(
-                "font-bold",
-                "flex",
-                "items-center",
-                "mb-2",
-                "text-primary-500"
-              )}
-            >
-              <MailIcon className={classNames("inline-icon", "mr-2")} />
-              {props.customer?.email || "Email-Adresse fehlt"}
-            </div>
+          <>
+            <Box>
+              <div
+                className={classNames(
+                  "font-bold",
+                  "flex",
+                  "items-center",
+                  "text-primary-500"
+                )}
+              >
+                <UserIcon className={classNames("inline-icon", "mr-2")} />
+                {props.customer?.name}
+              </div>
+              <div
+                className={classNames(
+                  "font-bold",
+                  "flex",
+                  "items-center",
+                  "mb-2",
+                  "text-primary-500"
+                )}
+              >
+                <MailIcon className={classNames("inline-icon", "mr-2")} />
+                {props.customer?.email || "Email-Adresse fehlt"}
+              </div>
 
-            {
-              <div className={classNames("flex", "items-center")}>
-                <OfficeBuildingIcon
-                  className={classNames("inline-icon", "mr-2")}
-                />
-                <div>
-                  {props.customer?.company || "Firma fehlt"}
-                  <div className={classNames("text-gray-500")}>
-                    {props.customer?.address || "Adresse fehlt"}
-                  </div>
-                  <div className={classNames("text-gray-500")}>
-                    {props.customer?.zip || "PLZ fehlt"} &#183;{" "}
-                    {props.customer?.city || "Stadt fehlt"}
-                  </div>
-                  <div className={classNames("text-gray-500")}>
-                    {props.customer?.country || "Land fehlt"}
+              {
+                <div className={classNames("flex", "items-center")}>
+                  <OfficeBuildingIcon
+                    className={classNames("inline-icon", "mr-2")}
+                  />
+                  <div>
+                    {props.customer?.company || "Firma fehlt"}
+                    <div className={classNames("text-gray-500")}>
+                      {props.customer?.address || "Adresse fehlt"}
+                    </div>
+                    <div className={classNames("text-gray-500")}>
+                      {props.customer?.zip || "PLZ fehlt"} &#183;{" "}
+                      {props.customer?.city || "Stadt fehlt"}
+                    </div>
+                    <div className={classNames("text-gray-500")}>
+                      {props.customer?.country || "Land fehlt"}
+                    </div>
                   </div>
                 </div>
-              </div>
-            }
-          </Box>
+              }
+            </Box>
+            <CreateOrEditCustomerForm customer={props.customer} />
+          </>
         )}
-        <CreateOrEditCustomerForm customer={props.customer || undefined} />
       </Section>
       <Section
         title="Aufträge"
