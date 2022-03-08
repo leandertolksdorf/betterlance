012d61ffa76de5bc271d7534f47eca7a8fab8fc4
Leander Tolksdorf <leander.tolksdorf@fu-berlin.de>
2022-03-09 00:27:29 +0100

    Section: adjust text style

--- src/components/Section/view.tsx
+++ src/components/Section/view.tsx
@@ -5,11 +5,15 @@ export const SectionView = (props: SectionProps) => {
     <div className={classNames("mb-4", "grid", "grid-cols-3", "gap-8")}>
       <div className={classNames("col-span-1")}>
         {props.title && (
-          <h2 className={classNames("text-primary-900", "font-bold", "mb-1")}>
+          <h2 className={classNames("text-primary-500", "font-bold")}>
             {props.title}
           </h2>
         )}
-        {props.text && <p className={classNames("")}>{props.text}</p>}
+        {props.text && (
+          <p className={classNames("text-primary-900", "font-bold")}>
+            {props.text}
+          </p>
+        )}
       </div>
       <div className={classNames("col-span-2")}>{props.children}</div>
     </div>
