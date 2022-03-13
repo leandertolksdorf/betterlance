21d2924e8f106ad251ec9d06e31d35bb6dcc7437
Leander Tolksdorf <leander.tolksdorf@fu-berlin.de>
2022-03-13 22:01:23 +0100

    Section: add wide mode

--- src/components/Section/index.tsx
+++ src/components/Section/index.tsx
@@ -4,6 +4,7 @@ import { SectionView } from "./view";
 export type SectionProps = {
   title?: string;
   text?: string;
+  wide?: boolean;
   children?: ReactNode;
   wrapChild?: boolean;
 };
--- src/components/Section/view.tsx
+++ src/components/Section/view.tsx
@@ -2,8 +2,15 @@ import classNames from "classnames";
 import { SectionProps } from ".";
 export const SectionView = (props: SectionProps) => {
   return (
-    <div className={classNames("mb-4", "grid", "grid-cols-3", "gap-8")}>
-      <div className={classNames("col-span-1")}>
+    <div
+      className={classNames(
+        "mb-4",
+        "grid",
+        "grid-cols-3",
+        props.wide ? "gap-2" : "gap-8"
+      )}
+    >
+      <div className={classNames(props.wide ? "col-span-3" : "col-span-1")}>
         {props.title && (
           <h2 className={classNames("text-primary-500", "font-bold")}>
             {props.title}
@@ -15,7 +22,9 @@ export const SectionView = (props: SectionProps) => {
           </p>
         )}
       </div>
-      <div className={classNames("col-span-2")}>{props.children}</div>
+      <div className={classNames(props.wide ? "col-span-3" : "col-span-2")}>
+        {props.children}
+      </div>
     </div>
   );
 };
