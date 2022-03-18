630a8c751b04c25a6b68cfdd19aef3eabc808dab
Leander Tolksdorf <leander.tolksdorf@fu-berlin.de>
2022-03-18 23:56:24 +0100

    UpsertProjectForm: use Collapse component (#12)

--- src/components/UpsertProjectForm/view.tsx
+++ src/components/UpsertProjectForm/view.tsx
@@ -7,6 +7,7 @@ import { FieldErrors, UseFormRegister } from "react-hook-form";
 import { FormData, UpsertProjectFormProps } from ".";
 import { definitions } from "../../types/supabase";
 import { Button } from "../Button";
+import { Collapse } from "../Collapse";
 import { DimExcept } from "../DimExcept";
 
 type UpsertProjectFormViewProps = UpsertProjectFormProps & {
@@ -30,88 +31,69 @@ export const UpsertProjectFormView = (props: UpsertProjectFormViewProps) => {
   });
 
   return (
-    <DimExcept dim={props.isOpen}>
-      <div className={classNames("bg-gray-100", "rounded-lg", "my-2")}>
-        <Button
-          light={props.isOpen}
-          onClick={props.onOpen}
-          icon={props.isOpen ? <XIcon /> : <PlusIcon />}
-        >
-          {props.isOpen
-            ? "Schließen"
-            : props.project
-            ? "Projekt bearbeiten"
-            : "Projekt anlegen"}
-        </Button>
-        <div
-          ref={innerRef}
-          className={classNames("overflow-hidden", "transition-[max-height]")}
-          style={{ maxHeight: props.isOpen ? innerHeight : 0 }}
-        >
-          <div className={classNames("p-4")}>
-            <h3 className={classNames("mb-2")}>
-              {props.project ? "Auftrag bearbeiten" : "Auftrag anlegen"}
-            </h3>
-            <form onSubmit={props.onSubmit}>
-              <label>
-                Name{" "}
-                <ErrorMessage
-                  errors={props.errors}
-                  name="name"
-                  render={({ message }) => (
-                    <span className={classNames("text-red-600", "ml-1")}>
-                      <ExclamationIcon
-                        className={classNames("inline-icon", "mr-1")}
-                      />
-                      {message}
-                    </span>
-                  )}
-                />
-                <input {...props.register("name")} />
-              </label>
-              <label>
-                Kunde{" "}
-                <ErrorMessage
-                  errors={props.errors}
-                  name="customer"
-                  render={({ message }) => (
-                    <span className={classNames("text-red-600", "ml-1")}>
-                      <ExclamationIcon
-                        className={classNames("inline-icon", "mr-1")}
-                      />
-                      {message}
-                    </span>
-                  )}
-                />
-                <select {...props.register("customer", { required: false })}>
-                  <option value={""}>keinen Kunden verknüpfen</option>
-                  {props.customers?.map((customer, index) => (
-                    <option key={index} value={customer.id}>
-                      {[customer.name, customer.company]
-                        .filter((value) => value !== "")
-                        .join(" · ")}
-                    </option>
-                  ))}
-                </select>
-              </label>
-              <Button type="submit" center loading={props.loading}>
-                Absenden
-              </Button>
-              {props.message && (
-                <div
-                  className={classNames(
-                    props.error ? "text-red-600" : "text-primary-500",
-                    "mt-2",
-                    "font-bold"
-                  )}
-                >
-                  {props.message}
-                </div>
+    <Collapse dim openText="Auftrag anlegen" closeText="Schließen">
+      <div className={classNames("p-4")}>
+        <h3 className={classNames("mb-2")}>
+          {props.project ? "Auftrag bearbeiten" : "Auftrag anlegen"}
+        </h3>
+        <form onSubmit={props.onSubmit}>
+          <label>
+            Name{" "}
+            <ErrorMessage
+              errors={props.errors}
+              name="name"
+              render={({ message }) => (
+                <span className={classNames("text-red-600", "ml-1")}>
+                  <ExclamationIcon
+                    className={classNames("inline-icon", "mr-1")}
+                  />
+                  {message}
+                </span>
               )}
-            </form>
-          </div>
-        </div>
+            />
+            <input {...props.register("name")} />
+          </label>
+          <label>
+            Kunde{" "}
+            <ErrorMessage
+              errors={props.errors}
+              name="customer"
+              render={({ message }) => (
+                <span className={classNames("text-red-600", "ml-1")}>
+                  <ExclamationIcon
+                    className={classNames("inline-icon", "mr-1")}
+                  />
+                  {message}
+                </span>
+              )}
+            />
+            <select {...props.register("customer", { required: false })}>
+              <option value={""}>keinen Kunden verknüpfen</option>
+              {props.customers?.map((customer, index) => (
+                <option key={index} value={customer.id}>
+                  {[customer.name, customer.company]
+                    .filter((value) => value !== "")
+                    .join(" · ")}
+                </option>
+              ))}
+            </select>
+          </label>
+          <Button type="submit" center loading={props.loading}>
+            Absenden
+          </Button>
+          {props.message && (
+            <div
+              className={classNames(
+                props.error ? "text-red-600" : "text-primary-500",
+                "mt-2",
+                "font-bold"
+              )}
+            >
+              {props.message}
+            </div>
+          )}
+        </form>
       </div>
-    </DimExcept>
+    </Collapse>
   );
 };
