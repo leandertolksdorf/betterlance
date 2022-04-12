6017e2ddbd12365f4d4e338f1bcdc1be173efe4c
Leander Tolksdorf <leander.tolksdorf@fu-berlin.de>
2022-04-12 13:07:22 +0200

    remove unused composite types

--- src/types/composite.ts
+++ src/types/composite.ts
@@ -1,12 +1,4 @@
 import { definitions } from "./supabase";
-
-export type ProjectWithCustomer = Omit<definitions["project"], "customer"> & {
-  customer?: definitions["customer"];
-};
-
-export type TaskWithProject = Omit<definitions["task"], "project"> & {
-  project?: definitions["project"];
-};
 export type Project = Omit<definitions["project"], "customer"> & {
   customer: definitions["customer"];
 };
